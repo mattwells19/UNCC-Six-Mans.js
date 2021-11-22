4307f114135bf8edfa4758dbe6e4f8aac1f0edf3
mattwells19 <mwells21@uncc.edu>
2021-11-22 18:08:31 -0500

    Remove unnecessary if and pull out reusable function

--- src/controllers/LeaderboardChannelController.ts
+++ src/controllers/LeaderboardChannelController.ts
@@ -1,20 +1,19 @@
 import { MessageEmbed, TextChannel } from "discord.js";
 import { LeaderboardToString } from "../services/LeaderboardService";
 import Colors from "../utils/colors";
+import deleteAllMessagesInTextChannel from "../utils/deleteAllMessagesInTextChannel";
 
 export async function updateLeaderboardChannel(leaderboardChannel: TextChannel): Promise<void> {
   const leaderboardContent = await LeaderboardToString();
 
-  if (leaderboardChannel) {
-    await leaderboardChannel.messages.fetch({ limit: 99 }).then((messages) => leaderboardChannel.bulkDelete(messages));
+  await deleteAllMessagesInTextChannel(leaderboardChannel);
 
-    const embeds = leaderboardContent.map((content, index) =>
-      new MessageEmbed()
-        .setColor(Colors.Blue)
-        .setTitle(`UNCC 6 Mans | Leaderboard ${index + 1}/${leaderboardContent.length}`)
-        .setDescription("```" + content + "```")
-    );
+  const embeds = leaderboardContent.map((content, index) =>
+    new MessageEmbed()
+      .setColor(Colors.Blue)
+      .setTitle(`UNCC 6 Mans | Leaderboard ${index + 1}/${leaderboardContent.length}`)
+      .setDescription("```" + content + "```")
+  );
 
-    await leaderboardChannel.send({ embeds });
-  }
+  await leaderboardChannel.send({ embeds });
 }
