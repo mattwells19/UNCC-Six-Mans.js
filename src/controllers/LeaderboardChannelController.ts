449cc886b99a904e6f0765923cbf1df63c279f16
Duane Kord <duanekord@gmail.com>
2022-02-11 12:00:53 -0500

    Fixing ESLint issues.
    Removed hardcoded embeds in favor of the MessageBuilder.

--- src/controllers/LeaderboardChannelController.ts
+++ src/controllers/LeaderboardChannelController.ts
@@ -6,19 +6,8 @@ import MessageBuilder from "../repositories/helpers/MessageBuilder";
 export async function updateLeaderboardChannel(leaderboardChannel: TextChannel): Promise<void> {
 
   const leaderboardContent = await LeaderboardToString();
-  const normIconURL = "https://raw.githubusercontent.com/mattwells19/UNCC-Six-Mans.js/main/media/norm_still.png";
 
   await deleteAllMessagesInTextChannel(leaderboardChannel);
-
-  const embeds = leaderboardContent.map((content, index) => {
-    const embedCtr = leaderboardContent.length > 1 ? `(${index + 1}/${leaderboardContent.length})` : "";
-
-    return new MessageEmbed()
-      .setColor("BLUE")
-      .setTitle(`UNCC 6 Mans | Leaderboard ${embedCtr}`.trim())
-      .setDescription("```" + content + "```")
-      .setThumbnail(normIconURL);
-  });
 
   await leaderboardChannel.send({ embeds: MessageBuilder.leaderboardMessage(leaderboardContent) });
 }
