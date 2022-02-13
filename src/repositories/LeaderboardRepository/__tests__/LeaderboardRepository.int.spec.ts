e0f3ee2afa504c89ecd48c8a0ea8615f01566854
Matt Wells <mattwells878@gmail.com>
2022-02-13 12:11:48 -0500

    Update leaderboard tests

--- src/repositories/LeaderboardRepository/__tests__/LeaderboardRepository.int.spec.ts
+++ src/repositories/LeaderboardRepository/__tests__/LeaderboardRepository.int.spec.ts
@@ -1,17 +1,37 @@
-import LeaderboardRepository from "../LeaderboardRepository";
+import { PrismaClient } from "@prisma/client";
 import * as faker from "faker";
-import { BallChaser } from "../../../types/common";
-import { PlayerStats } from "../types";
 import { BallChaserBuilder } from "../../../../.jest/Builder";
-import NotionClient from "../../helpers/NotionClient";
+import { BallChaser } from "../../../types/common";
+import getEnvVariable from "../../../utils/getEnvVariable";
+import LeaderboardRepository from "../LeaderboardRepository";
+import { PlayerStats } from "../types";
 
-// set timeout to be longer (20 seconds) since async requests take extra time
-jest.setTimeout(20000);
+let prisma: PrismaClient;
+let seasonSemester: string;
+let seasonYear: string;
 
-async function removeAllPlayersFromLeaderboard() {
-  const notionClient = new NotionClient(process.env.notion_leaderboard_id ?? "");
-  await notionClient.findAllAndRemove();
-}
+beforeEach(async () => {
+  jest.clearAllMocks();
+});
+
+beforeAll(async () => {
+  seasonSemester = getEnvVariable("season_semester");
+  seasonYear = getEnvVariable("season_year");
+
+  prisma = new PrismaClient();
+  await prisma.$connect();
+  await prisma.leaderboard.deleteMany();
+  await prisma.ballChaser.deleteMany();
+});
+
+afterEach(async () => {
+  await prisma.leaderboard.deleteMany();
+  await prisma.ballChaser.deleteMany();
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
+});
 
 function makePlayerStats(
   ballChaser: BallChaser = BallChaserBuilder.single(),
@@ -32,79 +52,168 @@ function makePlayerStats(
   };
 }
 
-describe("Leaderboard Repository integration tests", () => {
-  afterEach(async () => {
-    await removeAllPlayersFromLeaderboard();
+const validatePlayerStats = (expected: PlayerStats, actual: PlayerStats | null) => {
+  expect(actual).not.toBeNull();
+  expect(actual!.id).toBe(expected.id);
+  expect(actual!.losses).toBe(expected.losses);
+  expect(actual!.matchesPlayed).toBe(expected.matchesPlayed);
+  expect(actual!.mmr).toBe(expected.mmr);
+  expect(actual!.name).toBe(expected.name);
+  expect(actual!.winPerc).toBe(expected.winPerc);
+  expect(actual!.wins).toBe(expected.wins);
+};
+
+async function manuallyAddPlayerStatsToLeaderboard(ballChaser: PlayerStats) {
+  await prisma.ballChaser.create({
+    data: {
+      id: ballChaser.id,
+      name: ballChaser.name,
+      rank: {
+        create: {
+          mmr: ballChaser.mmr,
+          losses: ballChaser.losses,
+          wins: ballChaser.wins,
+          seasonSemester,
+          seasonYear,
+        },
+      },
+    },
+  });
+}
+
+async function manuallyAddBallChaser(ballChaser: Pick<BallChaser, "id" | "name">) {
+  await prisma.ballChaser.create({
+    data: {
+      id: ballChaser.id,
+      name: ballChaser.name,
+    },
+  });
+}
+
+describe("LeaderboardRepository tests", () => {
+  it("gets player's stats", async () => {
+    const mockPlayerStats = makePlayerStats();
+    await manuallyAddPlayerStatsToLeaderboard(mockPlayerStats);
+
+    const result = await LeaderboardRepository.getPlayerStats(mockPlayerStats.id);
+
+    validatePlayerStats(mockPlayerStats, result);
   });
 
-  it("gets player by id", async () => {
-    const playerToAdd = makePlayerStats();
-    await LeaderboardRepository.updatePlayersStats([playerToAdd]);
-    const foundPlayer = await LeaderboardRepository.getPlayerStats(playerToAdd.id);
-
-    expect(foundPlayer).not.toBeNull();
-    expect(foundPlayer!.id).toBe(playerToAdd.id);
-    expect(foundPlayer!.losses).toBe(playerToAdd.losses);
-    expect(foundPlayer!.matchesPlayed).toBe(playerToAdd.matchesPlayed);
-    expect(foundPlayer!.mmr).toBe(playerToAdd.mmr);
-    expect(foundPlayer!.name).toBe(playerToAdd.name);
-    // formulas are fixed to one decimal place in NotionHelper so need to replicate here for equality
-    expect(foundPlayer!.winPerc).toBe(+playerToAdd.winPerc.toFixed(2));
-    expect(foundPlayer!.wins).toBe(playerToAdd.wins);
+  it("returns null when looking for player that does not exist", async () => {
+    const result = await LeaderboardRepository.getPlayerStats(faker.datatype.number());
+    expect(result).toBeNull();
   });
 
-  it("can add a player's stats and retrieve them in order based on MMR", async () => {
+  it("updates player stats when the player exists", async () => {
+    const mockPlayerStats = makePlayerStats();
+    await manuallyAddPlayerStatsToLeaderboard(mockPlayerStats);
+
+    const mockPlayerUpdates = makePlayerStats(undefined, { id: mockPlayerStats.id, name: mockPlayerStats.name });
+
+    await LeaderboardRepository.updatePlayersStats([mockPlayerUpdates]);
+
+    const actual = await prisma.leaderboard.findUnique({
+      include: {
+        player: true,
+      },
+      where: {
+        seasonSemester_seasonYear_playerId: {
+          playerId: mockPlayerStats.id,
+          seasonSemester,
+          seasonYear,
+        },
+      },
+    });
+
+    expect(actual).not.toBeNull();
+
+    // should not change
+    expect(actual?.player.id).toBe(mockPlayerStats.id);
+    expect(actual?.player.name).toBe(mockPlayerStats.name);
+
+    // should change
+    expect(actual?.mmr).toBe(mockPlayerUpdates.mmr);
+    expect(actual?.wins).toBe(mockPlayerUpdates.wins);
+    expect(actual?.losses).toBe(mockPlayerUpdates.losses);
+  });
+
+  it("adds a player's stats when the player does not already exist", async () => {
+    const mockPlayerStats = makePlayerStats();
+    await manuallyAddBallChaser(mockPlayerStats);
+
+    await LeaderboardRepository.updatePlayersStats([mockPlayerStats]);
+
+    const actual = await prisma.leaderboard.findUnique({
+      include: {
+        player: true,
+      },
+      where: {
+        seasonSemester_seasonYear_playerId: {
+          playerId: mockPlayerStats.id,
+          seasonSemester,
+          seasonYear,
+        },
+      },
+    });
+
+    expect(actual).not.toBeNull();
+
+    expect(actual?.player.id).toBe(mockPlayerStats.id);
+    expect(actual?.player.name).toBe(mockPlayerStats.name);
+    expect(actual?.mmr).toBe(mockPlayerStats.mmr);
+    expect(actual?.wins).toBe(mockPlayerStats.wins);
+    expect(actual?.losses).toBe(mockPlayerStats.losses);
+  });
+
+  it("gets top n player stats", async () => {
     const playersToAdd = Array.from({ length: 10 }, () => makePlayerStats());
-
-    await LeaderboardRepository.updatePlayersStats(playersToAdd);
-    const allPlayers = await LeaderboardRepository.getAllPlayersStats();
-
-    expect(allPlayers).toHaveLength(10);
-    for (let i = 0; i < 10 - 1; i++) {
-      expect(allPlayers[i].mmr).toBeGreaterThan(allPlayers[i + 1].mmr);
+    const promises = [];
+    for (const mockPlayerStats of playersToAdd) {
+      promises.push(manuallyAddPlayerStatsToLeaderboard(mockPlayerStats));
     }
-  });
+    await Promise.all(promises);
 
-  it("can add a player's stats and get the top 5 players based on MMR", async () => {
-    const playersToAdd = Array.from({ length: 10 }, () => makePlayerStats());
-
-    await LeaderboardRepository.updatePlayersStats(playersToAdd);
-    const allPlayers = await LeaderboardRepository.getTopNPlayersStats(5);
+    const allPlayers = await LeaderboardRepository.getPlayersStats(5);
 
     expect(allPlayers).toHaveLength(5);
+    // 5 - 1 since you can't [i + 1] on the last item
     for (let i = 0; i < 5 - 1; i++) {
       expect(allPlayers[i].mmr).toBeGreaterThan(allPlayers[i + 1].mmr);
     }
   });
 
-  it("update a player's stats if they're already on the leaderboard", async () => {
+  it("gets all player stats sorted correctly based on MMR", async () => {
     const playersToAdd = Array.from({ length: 10 }, () => makePlayerStats());
-    await LeaderboardRepository.updatePlayersStats(playersToAdd);
+    const promises = [];
+    for (const mockPlayerStats of playersToAdd) {
+      promises.push(manuallyAddPlayerStatsToLeaderboard(mockPlayerStats));
+    }
+    await Promise.all(promises);
 
-    const allPlayersBefore = await LeaderboardRepository.getAllPlayersStats();
-    expect(allPlayersBefore).toHaveLength(10);
+    const allPlayers = await LeaderboardRepository.getPlayersStats();
 
-    const playerUpdates = playersToAdd.map((p) => {
-      return makePlayerStats(undefined, { id: p.id, name: p.name });
-    });
+    expect(allPlayers).toHaveLength(playersToAdd.length);
+    // playersToAdd.length - 1 since you can't [i + 1] on the last item
+    for (let i = 0; i < playersToAdd.length - 1; i++) {
+      expect(allPlayers[i].mmr).toBeGreaterThan(allPlayers[i + 1].mmr);
+    }
+  });
 
-    await LeaderboardRepository.updatePlayersStats(playerUpdates);
+  it("gets all player stats sorted correctly by wins when MMR is equal", async () => {
+    const playersToAdd = Array.from({ length: 5 }, () => makePlayerStats(undefined, { mmr: 100 }));
+    const promises = [];
+    for (const mockPlayerStats of playersToAdd) {
+      promises.push(manuallyAddPlayerStatsToLeaderboard(mockPlayerStats));
+    }
+    await Promise.all(promises);
 
-    const allPlayersAfter = await LeaderboardRepository.getAllPlayersStats();
-    expect(allPlayersAfter).toHaveLength(10);
+    const allPlayers = await LeaderboardRepository.getPlayersStats();
 
-    allPlayersAfter.forEach((actual) => {
-      const expected = playerUpdates.find((p) => p.id === actual.id);
-
-      expect(actual).not.toBeNull();
-      expect(actual.id).toBe(expected!.id);
-      expect(actual.losses).toBe(expected!.losses);
-      expect(actual.matchesPlayed).toBe(expected!.matchesPlayed);
-      expect(actual.mmr).toBe(expected!.mmr);
-      expect(actual.name).toBe(expected!.name);
-      // formulas are fixed to one decimal place in NotionHelper so need to replicate here for equality
-      expect(actual.winPerc).toBe(+expected!.winPerc.toFixed(2));
-      expect(actual.wins).toBe(expected!.wins);
-    });
+    expect(allPlayers).toHaveLength(playersToAdd.length);
+    // playersToAdd.length - 1 since you can't [i + 1] on the last item
+    for (let i = 0; i < playersToAdd.length - 1; i++) {
+      expect(allPlayers[i].wins).toBeGreaterThan(allPlayers[i + 1].wins);
+    }
   });
 });
