22ae506961b405352581cdd9231cfacee15305fd
Austin Baker <noise.9no@gmail.com>
2022-02-14 13:55:06 -0500

    Made Pretty Again - New Line @ End of File

--- src/controllers/Interactions.ts
+++ src/controllers/Interactions.ts
@@ -6,76 +6,76 @@ import { BallChaser } from "../types/common";
 import MessageBuilder from "../utils/MessageBuilder";
 
 export async function getCurrentQueue(queueChannel: TextChannel): Promise<void> {
+  
+  const ballchasers = await QueueRepository.getAllBallChasersInQueue();
 
-    const ballchasers = await QueueRepository.getAllBallChasersInQueue();
-
-    await queueChannel.send({
-        components: [MessageBuilder.queueButtons],
-        embeds: [MessageBuilder.queueMessage(ballchasers)]
-    });
+  await queueChannel.send({ 
+    components: [MessageBuilder.queueButtons],
+    embeds: [MessageBuilder.queueMessage(ballchasers)]
+  });
 }
 
 export async function onInteraction(buttonInteraction: Interaction, queueChannel: TextChannel): Promise<void> {
 
-    if (!buttonInteraction.isButton()) return;
-    await buttonInteraction.deferReply();
+  if (!buttonInteraction.isButton()) return;
+  await buttonInteraction.deferReply();
 
-    switch (buttonInteraction.customId) {
-        case "joinQueue": {
+  switch (buttonInteraction.customId) {
+    case "joinQueue": {
 
-            const queueMember = await QueueRepository.getBallChaserInQueue(buttonInteraction.user.id);
-            const leaderboardMember = await LeaderboardRepository.getPlayerStats(buttonInteraction.user.id);
+      const queueMember = await QueueRepository.getBallChaserInQueue(buttonInteraction.user.id);
+      const leaderboardMember = await LeaderboardRepository.getPlayerStats(buttonInteraction.user.id);
 
-            if (!queueMember) {
-                const player: BallChaser = {
-                    id: buttonInteraction.user.id,
-                    isCap: false,
-                    mmr: leaderboardMember ? leaderboardMember.mmr : 100,
-                    name: buttonInteraction.user.username,
-                    queueTime: DateTime.now().plus({ hours: 1 }),
-                    team: null
-                };
-                await QueueRepository.addBallChaserToQueue(player);
-            }
+      if (!queueMember) {
+        const player: BallChaser = {
+          id: buttonInteraction.user.id,
+          isCap: false,
+          mmr: leaderboardMember ? leaderboardMember.mmr : 100,
+          name: buttonInteraction.user.username,
+          queueTime: DateTime.now().plus({hours: 1}),
+          team: null
+        };
+        await QueueRepository.addBallChaserToQueue(player);
+      }
 
-            const ballchasers = await QueueRepository.getAllBallChasersInQueue();
+      const ballchasers = await QueueRepository.getAllBallChasersInQueue();
 
-            queueChannel.messages.delete(buttonInteraction.message.id);
+      queueChannel.messages.delete(buttonInteraction.message.id);
 
-            await buttonInteraction.editReply({
-                components: [MessageBuilder.queueButtons],
-                embeds: [MessageBuilder.queueMessage(ballchasers)]
-            });
-            break;
-        }
-
-        case "leaveQueue": {
-
-            const member = await QueueRepository.getBallChaserInQueue(buttonInteraction.user.id);
-
-            if (member != null) {
-                await QueueRepository.removeBallChaserFromQueue(buttonInteraction.user.id);
-
-                const remainingMembers = await QueueRepository.getAllBallChasersInQueue();
-
-                queueChannel.messages.delete(buttonInteraction.message.id);
-
-                await buttonInteraction.editReply({
-                    components: [MessageBuilder.queueButtons],
-                    embeds: [MessageBuilder.queueMessage(remainingMembers)]
-                });
-
-            } else {
-                const ballchasers = await QueueRepository.getAllBallChasersInQueue();
-
-                queueChannel.messages.delete(buttonInteraction.message.id);
-
-                await buttonInteraction.editReply({
-                    components: [MessageBuilder.queueButtons],
-                    embeds: [MessageBuilder.queueMessage(ballchasers)]
-                });
-            }
-            break;
-        }
+      await buttonInteraction.editReply({ 
+        components: [MessageBuilder.queueButtons],
+        embeds: [MessageBuilder.queueMessage(ballchasers)]
+      });
+      break;
     }
+
+    case "leaveQueue": {
+
+      const member = await QueueRepository.getBallChaserInQueue(buttonInteraction.user.id);
+
+      if (member != null) {
+        await QueueRepository.removeBallChaserFromQueue(buttonInteraction.user.id);
+
+        const remainingMembers = await QueueRepository.getAllBallChasersInQueue();
+
+        queueChannel.messages.delete(buttonInteraction.message.id);
+
+        await buttonInteraction.editReply({ 
+          components: [MessageBuilder.queueButtons],
+          embeds: [MessageBuilder.queueMessage(remainingMembers)]
+        });
+
+      } else {
+        const ballchasers = await QueueRepository.getAllBallChasersInQueue();
+
+        queueChannel.messages.delete(buttonInteraction.message.id);
+
+        await buttonInteraction.editReply({
+          components: [MessageBuilder.queueButtons],
+          embeds: [MessageBuilder.queueMessage(ballchasers)]
+        });
+      }
+      break;
+    }
+  }
 }
