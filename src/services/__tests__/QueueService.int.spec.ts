96bae4805499a7b60f156d59627da9b14be84e4f
Austin Baker <noise.9no@gmail.com>
2022-02-16 20:52:36 -0500

    Change to QueueService integration test

--- src/services/__tests__/QueueService.int.spec.ts
+++ src/services/__tests__/QueueService.int.spec.ts
@@ -1,19 +1,10 @@
 import QueueRepository from "../../repositories/QueueRepository";
 import { joinQueue } from "../QueueService";
 import { leaveQueue } from "../QueueService";
-import { mocked } from "ts-jest/utils";
 import { BallChaser } from "../../types/common";
 import { BallChaserBuilder } from "../../../.jest/Builder";
 import faker from "faker";
 import { DateTime } from "luxon";
-
-jest.mock("../../repositories/QueueRepository");
-jest.mock("../../repositories/LeaderboardRepository");
-jest.mock("../../utils/getEnvVariable");
-
-beforeEach(() => {
-  jest.clearAllMocks();
-});
 
 
 function makeBallChaser(
@@ -26,7 +17,7 @@ function makeBallChaser(
 
   return {
     id: ballChaser.id,
-    mmr: faker.datatype.number({ min: 0 }),
+    mmr: ballChaser.mmr,
     name: ballChaser.name,
     isCap: false,
     team: null,
@@ -40,19 +31,24 @@ const mockPlayer1 = makeBallChaser()
 describe("QueueService tests", () => {
   describe("Joining queue", () => {
     describe("Not already in queue", () => {
-      beforeEach(() => {
-        mocked(QueueRepository.getBallChaserInQueue).mockResolvedValue(null);
-      });
 
       it("joins a player to the queue if they are not already in the queue", async () => {
-        mocked(joinQueue(mockPlayer1.id, mockPlayer1.name));
+        joinQueue(mockPlayer1.id, mockPlayer1.name);
         const result = await joinQueue(mockPlayer1.id, mockPlayer1.name);
 
-        expect(result).toBe(QueueRepository.getAllBallChasersInQueue());
+        expect([result[0]]).toBe(Array<BallChaser>().length === 1);
         expect([result].length).toEqual([(await QueueRepository.getAllBallChasersInQueue())].length)
       });
 
-      it.todo("Not on leaderboard | joins queue with 100 MMR and queue time 1 hour from now");
+      it("Not on leaderboard | joins queue with 100 MMR and queue time 1 hour from now", async () => {
+        joinQueue(mockPlayer1.id, mockPlayer1.name);
+        const result = await joinQueue(mockPlayer1.id, mockPlayer1.name);
+        let mmr = result[0].mmr
+        let queueTime = result[0].queueTime
+
+        expect(mmr).toEqual(100);
+        expect(queueTime).toEqual(DateTime.now().plus({ minutes: 60 }))
+      });
       it.todo("On leaderboard | joins queue with Leaderboard MMR and queue time 1 hour from now");
     });
 
