c8261207dca71ece0a555cc131a0f408667049c0
Duane Kord <duanekord@gmail.com>
2022-02-20 22:28:49 -0500

    update tests

--- src/services/__tests__/TeamAssignmentService.unit.spec.ts
+++ src/services/__tests__/TeamAssignmentService.unit.spec.ts
@@ -3,11 +3,9 @@ import { Team } from "../../types/common";
 import { BallChaserQueueBuilder } from "../../../.jest/Builder";
 
 const mockBallChasers = BallChaserQueueBuilder.many(6);
-const mockMMRs = [23, 72, 117, 158, 162, 219];
+
 //Remove team values and mmr as they will be set below.
-mockBallChasers.forEach((player, index) => {
-  (player.team = null), (player.mmr = mockMMRs[index]);
-});
+mockBallChasers.forEach((player) => (player.team = null));
 
 describe("Team Assignment Service tests", () => {
   it("splits teams equally in size", async () => {
@@ -16,6 +14,7 @@ describe("Team Assignment Service tests", () => {
     expect(sortedBallChasers).toHaveLength(6);
     expect(sortedBallChasers.filter((player) => player.team == Team.Orange)).toHaveLength(3);
     expect(sortedBallChasers.filter((player) => player.team == Team.Blue)).toHaveLength(3);
+
     sortedBallChasers.forEach((player) => {
       expect(player.team).not.toBeNull();
     });
