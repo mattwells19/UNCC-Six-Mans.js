1b088b1742cbd936a519fa03ed0b413fb02d7c4f
Austin Baker <noise.9no@gmail.com>
2022-02-20 20:38:36 -0500

    QueueService.int Tests - Leave Queue Tests Small changes to other tests

--- src/services/__tests__/QueueService.int.spec.ts
+++ src/services/__tests__/QueueService.int.spec.ts
@@ -1,15 +1,12 @@
 import { joinQueue } from "../QueueService";
 import { leaveQueue } from "../QueueService";
-import { BallChaserQueueBuilder } from "../../../.jest/Builder";
+import { BallChaserQueueBuilder, LeaderboardBuilder } from "../../../.jest/Builder";
 import { DateTime } from "luxon";
 import getEnvVariable from "../../utils/getEnvVariable";
 import { PrismaClient } from "@prisma/client";
 import LeaderboardRepository from "../../repositories/LeaderboardRepository";
 import { mocked } from "ts-jest/utils";
-import { PlayerInQueue } from "../../repositories/QueueRepository/types";
-import { PlayerStats } from "../../repositories/LeaderboardRepository/types";
-import faker from "faker";
-import { LeaderboardToString } from "../LeaderboardService";
+import { join } from "path/posix";
 
 jest.mock("../../utils/getEnvVariable");
 jest.mock("../../repositories/LeaderboardRepository");
@@ -60,27 +57,20 @@ describe("QueueService tests", () => {
         const result = await joinQueue(mockPlayer1.id, mockPlayer1.name);
         let mmr = result[0].mmr;
         let receivedQueueTime = result[0].queueTime.diffNow().as("minutes");
-        let expectedQueueTime = DateTime.now().plus({ minutes: 60 });
+        let expectedQueueTime = DateTime.now().plus({ minutes: 60 }).set({ millisecond: 0, second: 0 });
 
         expect(mmr).toBe(null);
         expect(Math.round(receivedQueueTime)).toEqual(Math.round(expectedQueueTime.diffNow().as("minutes")));
       });
       it("On leaderboard | joins queue with Leaderboard MMR and queue time 1 hour from now", async () => {
-        const mockPlayer2: PlayerStats = {
-          id: "player_id_2",
-          losses: 6,
-          matchesPlayed: 16,
-          mmr: 119,
-          name: "h",
-          winPerc: 0.63,
-          wins: 10,
-        };
+        const mockPlayer2 = LeaderboardBuilder.single();
         mocked(LeaderboardRepository.getPlayerStats).mockResolvedValue(mockPlayer2);
         const resultJoin = await joinQueue(mockPlayer2.id, mockPlayer2.name);
         let receivedQueueTime = resultJoin[0].queueTime.diffNow().as("minutes");
-        let expectedQueueTime = DateTime.now().plus({ minutes: 60 });
+        let expectedQueueTime = DateTime.now().plus({ minutes: 60 }).set({ millisecond: 0, second: 0 });
 
         expect(resultJoin[0].mmr).toEqual(mockPlayer2.mmr);
+        expect(resultJoin).toHaveLength(1);
         expect(Math.round(receivedQueueTime)).toEqual(Math.round(expectedQueueTime.diffNow().as("minutes")));
       });
     });
@@ -88,18 +78,28 @@ describe("QueueService tests", () => {
     it("Already in the queue | updates queue time to 1 hour from now", async () => {
       const firstJoin = await joinQueue(mockPlayer1.id, mockPlayer1.name);
       let receivedQueueTime1 = firstJoin[0].queueTime.diffNow().as("minutes");
-      let expectedQueueTime1 = DateTime.now().plus({ minutes: 60 });
+      let expectedQueueTime1 = DateTime.now().plus({ minutes: 60 }).set({ millisecond: 0, second: 0 });
       expect(Math.round(receivedQueueTime1)).toEqual(Math.round(expectedQueueTime1.diffNow().as("minutes")));
       const secondJoin = await joinQueue(mockPlayer1.id, mockPlayer1.name);
       let receivedQueueTime2 = secondJoin[0].queueTime.diffNow().as("minutes");
-      let expectedQueueTime2 = DateTime.now().plus({ minutes: 60 });
+      let expectedQueueTime2 = DateTime.now().plus({ minutes: 60 }).set({ millisecond: 0, second: 0 });
       expect(Math.round(receivedQueueTime2)).toEqual(Math.round(expectedQueueTime2.diffNow().as("minutes")));
       expect(receivedQueueTime1).not.toEqual(receivedQueueTime2);
     });
   });
 
   describe("Leaving queue", () => {
-    it.todo("removes player from queue when they exist");
-    it.todo("does nothing if player is not in queue");
+    it("removes player from queue when they exist", async () => {
+      const resultJoin = await joinQueue(mockPlayer1.id, mockPlayer1.name);
+      expect(resultJoin).toHaveLength(1);
+      expect(resultJoin[0].id).toEqual(mockPlayer1.id);
+      const resultLeave = await leaveQueue(mockPlayer1.id);
+      expect(resultLeave).toStrictEqual([]);
+      expect(resultLeave).not.toEqual(resultJoin);
+    });
+    it("does nothing if player is not in queue", async () => {
+      const result = await leaveQueue(mockPlayer1.id);
+      expect(result).toEqual(null);
+    });
   });
 });
