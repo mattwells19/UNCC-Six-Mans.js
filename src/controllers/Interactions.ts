cc4ae76e44f3a7d83d266e4ecbe204b9f070f5b4
Austin Baker <65349601+N0ise9@users.noreply.github.com>
2022-02-22 21:06:34 -0500

    Join and leave interactions (#12)
    * Reopen Branch
    
    * Removal of Buttons + Logic work for Join
    
    * Interactions.ts Join/Leave functionallity
    Change return type of QueueRepository.removeBallChaserFromQueue()
    
    * Changed To defer replies
    
    Co-authored-by: Duane Kord <duanekord@users.noreply.github.com>
    
    * Update to Join/Leave Functions - One Message Norm
    Co-authored-by: Duane Kord <duanekord@users.noreply.github.com>
    
    * Await Deffered Interactions
    
    * Creating MessageBuilder (#11)
    
    * Creating MessageBuilder
    
    * Updates to PR comments.
    
    * Uncommenting the thrown error
    
    * Fixing ESLint issues.  
    
    Removed hardcoded embeds in favor of the MessageBuilder.
    
    * PR fixes
    
    * Requested Changes on PR (#12)
    
    * Fixed Indentation, Made Pretty
    
    * Made Pretty Again - New Line @ End of File
    
    * Change to send Ephemeral message
    -Leaving while not in queue
    
    * Changed deferReply to deferUpdate
    Added Queue Times
    
    * Structural refactoring + simplifying MessageBuilder building
    
    * Edit to Queue Time to display 60 when joining
    
    * Disable Join/Leave Buttons on Interaction
    
    * Fix linting errors
    
    * Fix more linting errors
    
    * Finish test of joining when not in queue
    
    * Change to QueueService integration test
    
    * Fix to Leaving while not in queue button update
    
    * Update to Use Join/Leave functions with Prisma
    
    * Made Pretty - Fix Linting Errors
    
    * QueueService.int tests -
    On Leaderboard | Joins queue with LB MMR and 1hr queue time
    
    * Fix Linting Errors
    
    * QueueService.int tests -
    Already in queue | updates queue time to 1 hour from now
    
    * QueueService.int Tests -
    Leave Queue Tests
    Small changes to other tests
    
    * Requested Changes in (#12)
    
    * Fix Linting Errors
    
    * Requested Changes in (#12)
    
    * Added Method to support changes in (#12)
    enabledQueueButtons()
    Revertion of previously made changes in Interactions.ts
    
    Co-authored-by: Duane Kord <duanekord@users.noreply.github.com>
    Co-authored-by: Duane Kord <duanekord@gmail.com>
    Co-authored-by: Matt Wells <mattwells878@gmail.com>

---
+++ src/controllers/Interactions.ts
@@ -0,0 +1,38 @@
+import { Interaction, Message, TextChannel } from "discord.js";
+import QueueRepository from "../repositories/QueueRepository";
+import { joinQueue, leaveQueue } from "../services/QueueService";
+import MessageBuilder, { ButtonCustomID } from "../utils/MessageBuilder";
+
+export async function postCurrentQueue(queueChannel: TextChannel): Promise<void> {
+  const ballchasers = await QueueRepository.getAllBallChasersInQueue();
+  await queueChannel.send(MessageBuilder.queueMessage(ballchasers));
+}
+
+export async function handleInteraction(buttonInteraction: Interaction): Promise<void> {
+  if (!buttonInteraction.isButton()) return;
+
+  const message = buttonInteraction.message;
+  if (!(message instanceof Message)) return;
+
+  await buttonInteraction.deferUpdate();
+
+  switch (buttonInteraction.customId) {
+    case ButtonCustomID.JoinQueue: {
+      await message.edit(MessageBuilder.disabledQueueButtons(buttonInteraction));
+      const ballchasers = await joinQueue(buttonInteraction.user.id, buttonInteraction.user.username);
+      await message.edit(MessageBuilder.queueMessage(ballchasers));
+      break;
+    }
+    case ButtonCustomID.LeaveQueue: {
+      await message.edit(MessageBuilder.disabledQueueButtons(buttonInteraction));
+      const remainingMembers = await leaveQueue(buttonInteraction.user.id);
+
+      if (remainingMembers) {
+        await message.edit(MessageBuilder.queueMessage(remainingMembers));
+      } else {
+        await message.edit(MessageBuilder.enabledQueueButtons());
+      }
+      break;
+    }
+  }
+}
---
+++ src/utils/MessageBuilder.ts
@@ -0,0 +1,113 @@
+import { ButtonInteraction, MessageActionRow, MessageButton, MessageEmbed, MessageOptions } from "discord.js";
+import { PlayerInQueue } from "../repositories/QueueRepository/types";
+
+export const enum ButtonCustomID {
+  JoinQueue = "joinQueue",
+  LeaveQueue = "leaveQueue",
+}
+
+export default class MessageBuilder {
+  private static readonly normIconURL =
+    "https://raw.githubusercontent.com/mattwells19/UNCC-Six-Mans.js/main/media/norm_still.png";
+
+  static leaderboardMessage(leaderboardInfo: string[]): MessageOptions {
+    const embeds = leaderboardInfo.map((content, index) => {
+      const embedCtr = leaderboardInfo.length > 1 ? `(${index + 1}/${leaderboardInfo.length})` : "";
+
+      return new MessageEmbed({
+        color: "BLUE",
+        description: "```" + content + "```",
+        thumbnail: { url: this.normIconURL },
+        title: `UNCC 6 Mans | Leaderboard ${embedCtr}`.trim(),
+      });
+    });
+
+    return {
+      embeds,
+    };
+  }
+
+  static disabledQueueButtons(buttonInteraction: ButtonInteraction): MessageOptions {
+    const waitLabel = "Please wait...";
+    const joinButton = new MessageButton({
+      customId: ButtonCustomID.JoinQueue,
+      disabled: true,
+      label: "Join",
+      style: "SUCCESS",
+    });
+    const leaveButton = new MessageButton({
+      customId: ButtonCustomID.LeaveQueue,
+      disabled: true,
+      label: "Leave",
+      style: "DANGER",
+    });
+
+    switch (buttonInteraction.customId) {
+      case ButtonCustomID.JoinQueue: {
+        joinButton.label = waitLabel;
+        break;
+      }
+      case ButtonCustomID.LeaveQueue: {
+        leaveButton.label = waitLabel;
+        break;
+      }
+    }
+    return {
+      components: [new MessageActionRow({ components: [joinButton, leaveButton] })],
+    };
+  }
+
+  static enabledQueueButtons(): MessageOptions {
+    const joinButton = new MessageButton({
+      customId: ButtonCustomID.JoinQueue,
+      label: "Join",
+      style: "SUCCESS",
+    });
+    const leaveButton = new MessageButton({
+      customId: ButtonCustomID.LeaveQueue,
+      label: "Leave",
+      style: "DANGER",
+    });
+    return {
+      components: [new MessageActionRow({ components: [joinButton, leaveButton] })],
+    };
+  }
+
+  static queueMessage(ballchasers: ReadonlyArray<Readonly<PlayerInQueue>>): MessageOptions {
+    const embed = new MessageEmbed({
+      color: "GREEN",
+      thumbnail: { url: this.normIconURL },
+    });
+    const joinButton = new MessageButton({
+      customId: ButtonCustomID.JoinQueue,
+      label: "Join",
+      style: "SUCCESS",
+    });
+    const leaveButton = new MessageButton({
+      customId: ButtonCustomID.LeaveQueue,
+      label: "Leave",
+      style: "DANGER",
+    });
+
+    if (ballchasers.length == 0) {
+      embed.setTitle("Queue is Empty").setDescription("Click the green button to join the queue!");
+    } else {
+      const ballChaserList = ballchasers
+        .map((ballChaser) => {
+          // + 1 since it seems that joining the queue calculates to 59 instead of 60
+          const queueTime = ballChaser.queueTime?.diffNow().as("minutes") ?? 0;
+          return `${ballChaser.name} (${Math.min(queueTime + 1, 60).toFixed()} mins)`;
+        })
+        .join("\n");
+
+      embed
+        .setTitle(`Current Queue: ${ballchasers.length}/6`)
+        .setDescription("Click the green button to join the queue!\n\n" + ballChaserList);
+    }
+
+    return {
+      components: [new MessageActionRow({ components: [joinButton, leaveButton] })],
+      embeds: [embed],
+    };
+  }
+}
