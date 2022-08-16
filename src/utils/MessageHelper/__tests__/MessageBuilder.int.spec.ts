8a863ec0451ddd5e1abe52d1a5b488e07621c0ff
Duane Kord <duanekord@gmail.com>
2022-08-16 16:14:43 -0400

    Update to tests

--- src/utils/MessageHelper/__tests__/MessageBuilder.int.spec.ts
+++ src/utils/MessageHelper/__tests__/MessageBuilder.int.spec.ts
@@ -1,10 +1,16 @@
+import faker from "faker";
 import { BallChaserQueueBuilder } from "../../../../.jest/Builder";
+import { ActiveMatchCreated } from "../../../services/MatchService";
 import MessageBuilder from "../MessageBuilder";
+import { PlayerInActiveMatch } from "../../../repositories/ActiveMatchRepository/types";
+import { Team } from "../../../types/common";
 
 jest.mock("../../utils");
 
 describe("Building Buttons", () => {
   const mockBallChasers = BallChaserQueueBuilder.many(6);
+  const mockMatchId = faker.datatype.uuid();
+
   it("return queue buttons", () => {
     const result = MessageBuilder.queueMessage(mockBallChasers);
     expect(result).toMatchSnapshot();
@@ -17,8 +23,36 @@ describe("Building Buttons", () => {
     const result = MessageBuilder.captainChooseMessage(true, mockBallChasers);
     expect(result).toMatchSnapshot();
   });
-  it("return active match buttons", () => {
-    const activeMatch = mockBallChasers.map((p) => ({ id: p.id, team: p.team! }));
+  it("return active match buttons", async () => {
+    const orangePlayer: PlayerInActiveMatch = {
+      id: mockBallChasers[0].id,
+      team: Team.Orange,
+      reportedTeam: null,
+      matchId: mockMatchId,
+      mmr: 100,
+    };
+
+    const bluePlayer: PlayerInActiveMatch = {
+      id: mockBallChasers[1].id,
+      team: Team.Blue,
+      reportedTeam: null,
+      matchId: mockMatchId,
+      mmr: 100,
+    };
+
+    const activeMatch: ActiveMatchCreated = {
+      blue: {
+        mmrStake: 100,
+        players: [bluePlayer],
+        winProbability: 50,
+      },
+      orange: {
+        mmrStake: 100,
+        players: [orangePlayer],
+        winProbability: 50,
+      },
+    };
+
     const result = MessageBuilder.activeMatchMessage(activeMatch);
     expect(result).toMatchSnapshot();
   });
