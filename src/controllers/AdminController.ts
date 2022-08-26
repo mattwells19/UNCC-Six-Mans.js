9b93bcf9ab9dcf6025bcbcaa108c2da18c208ee0
Duane Kord <duanekord@gmail.com>
2022-08-26 15:21:19 -0400

    Fix commenting

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -18,20 +18,17 @@ export async function handleAdminInteraction(slashCommandInteraction: CommandInt
   const memberRoles = slashCommandInteraction.member?.roles;
   const newSeasonName = slashCommandInteraction.options.getString("new_season_name");
 
-  /*   if (
+  if (
     !memberRoles ||
     (memberRoles instanceof GuildMemberRoleManager && memberRoles.cache.every((role) => role.name !== "Bot Admin")) ||
     (Array.isArray(memberRoles) && !memberRoles.includes("Bot Admin"))
   ) {
     await slashCommandInteraction.editReply(
       // eslint-disable-next-line max-len
-      "What do you think you're doing? Trying to run an admin command when you're not a Bot Admin. Typical
-      How dare you even consider that my parents didn't think to check this
-      You think this is their first time writing code? Of course not
-      Now scram before I call my parents to come pick me up."
+      "What do you think you're doing? Trying to run an admin command when you're not a Bot Admin. Typical. How dare you even consider that my parents didn't think to check this.  You think this is their first time writing code? Of course not! Now scram before I call my parents to come pick me up."
     );
     return;
-  } */
+  }
 
   switch (slashCommandInteraction.commandName) {
     case AdminCommandOptions.Kick: {
